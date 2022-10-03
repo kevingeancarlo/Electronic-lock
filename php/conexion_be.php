2eda8ba7a3a9bab20e91c550f52f142a5f6e3475
Kevin <70453962+kevingeancarlo@users.noreply.github.com>
2022-10-03 15:47:35 -0500

    31022

---
+++ php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db", "3308");
+    
+    /* if ($conexion){
+        echo 'conectado con exito a la base de datos';
+    }else{
+        echo 'No se ha podido conectar a la base de datos';
+    } */
+
+?>
