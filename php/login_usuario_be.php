768d6b076fd054215f1383f7122aff82c2d2c279
Kevin <70453962+kevingeancarlo@users.noreply.github.com>
2022-10-08 15:31:38 -0500

    cambios

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -8,17 +8,6 @@
     $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo ='$correo'
     and contrasena =  '$contrasena'");
 
-    if(isset($_COOKIE["block".$correo]))
-    {
-        echo '
-                    <script>
-                        alert("Cerradura deshabilitada por 1 minuto para el usuario con $correo");
-                        window.location = "../index.php";
-                        
-                    </script>
-                ';
-                exit;
-    } else{
             if(mysqli_num_rows($validar_login) > 0 && $correo == "david@gmail.com"){
                 $_SESSION['usuario'] = $correo;
                 header("location: ../perfiles/perfil_david.php");
@@ -34,16 +23,13 @@
                 header("location: ../perfiles/perfil_laura.php");
                 exit;
             }else{
-                if(isset($_COOKIE["$correo"])){
-                    $cont = $_COOKIE["$correo"];
-                    $cont++;
-                    setcookie($correo,$cont,time()+ 120);
-                    if($cont >=2){
-                        setcookie("block".$correo,$cont,time()+60);
-                    }
-                }else{
-                    setcookie($correo,1,time()+120);
-                }
+                echo '
+                    <script>
+                        alert("Cerradura deshabilitada por 1 minuto para el usuario con $correo");
+                        window.location = "../index.php";
+                        
+                    </script>
+                ';
+                exit;
             }
-         }
 ?>
