efd8063b4c9ebea48b46576724b343bc89f398bf
Kevin <70453962+kevingeancarlo@users.noreply.github.com>
2022-10-08 15:29:35 -0500

    cambios

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -22,10 +22,10 @@
     $conexion = new mysqli($server, $username, $password, $db);
 
 
-    if ($conexion){
+    /* if ($conexion){
         echo 'conectado con exito a la base de datos';
     }else{
         echo 'No se ha podido conectar a la base de datos';
-    }  
+    }   */
 
 ?>
